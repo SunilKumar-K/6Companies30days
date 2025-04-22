35bfc19c49733c496967a9be8850c353e13f1299
Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>
2025-04-22 20:11:30 +0530

    Time: 1 ms (34.11%) | Memory: 41.8 MB (41.77%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,29 +1,48 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        int l = 0;
-        int r = s.length()-1;
-        char res[] = new char[s.length()];
 
-        while(l <= r)
+        StringBuilder str = new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--)
         {
-            if(Character.isAlphabetic(s.charAt(l))&& Character.isAlphabetic(s.charAt(r)))
+            if(Character.isAlphabetic(s.charAt(i)))
             {
-                res[r] = s.charAt(l);
-                res[l] = s.charAt(r);
-                l++;
-                r--;
-            }
-            else if(!Character.isAlphabetic(s.charAt(l))){
-                res[l]=s.charAt(l);
-                l++;
-            }
-            else if(!Character.isAlphabetic(s.charAt(r)))
-            {
-                res[r] = s.charAt(r);
-                r--;
+                str.append(s.charAt(i));
             }
         }
 
-        return new String(res);
+        for(int i=0;i<s.length();i++)
+        {
+            if(!Character.isAlphabetic(s.charAt(i)))
+            {
+                str.insert(i,s.charAt(i));
+            }
+        }
+
+        return str.toString();
+        // int l = 0;
+        // int r = s.length()-1;
+        // char res[] = new char[s.length()];
+
+        // while(l <= r)
+        // {
+        //     if(Character.isAlphabetic(s.charAt(l))&& Character.isAlphabetic(s.charAt(r)))
+        //     {
+        //         res[r] = s.charAt(l);
+        //         res[l] = s.charAt(r);
+        //         l++;
+        //         r--;
+        //     }
+        //     else if(!Character.isAlphabetic(s.charAt(l))){
+        //         res[l]=s.charAt(l);
+        //         l++;
+        //     }
+        //     else if(!Character.isAlphabetic(s.charAt(r)))
+        //     {
+        //         res[r] = s.charAt(r);
+        //         r--;
+        //     }
+        // }
+
+        // return new String(res);
     }
 }
