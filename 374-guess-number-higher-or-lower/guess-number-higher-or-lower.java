36e170889273a508f3ba1a0473fccd946d436c72
Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>
2025-06-17 22:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.73%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
@@ -1,49 +1,49 @@
-// /** 
-//  * Forward declaration of guess API.
-//  * @param  num   your guess
-//  * @return 	     -1 if num is higher than the picked number
-//  *			      1 if num is lower than the picked number
-//  *               otherwise return 0
-//  * int guess(int num);
-//  */
-
-// public class Solution extends GuessGame {
-//     public int guessNumber(int n) {
-//         int st = 0;
-//         int en = n;
-
-//         while(st <= en)
-//         {
-//             int mid = (st + en)/2;
-//             if( guess(mid) == 0) return mid;
-//             if(guess(mid) == -1){
-//                 en = mid -1;
-//             }
-//             if(guess(mid) == 1){
-//                 st = mid+1;
-//             }
-//         }
-
-//         return -1;
-//     }
-// }
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int low = 0;
-        int high = n;
+        int st = 0;
+        int en = n;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (guess(mid) == 0)
-                return mid;
-            if (guess(mid) == -1)
-                high = mid - 1;
-            if (guess(mid) == 1)
-                low = mid + 1;
+        while(st <= en)
+        {
+            int mid = st + (en-st)/2;
+            if( guess(mid) == 0) return mid;
+            if(guess(mid) == -1){
+                en = mid - 1;
+            }
+            if(guess(mid) == 1){
+                st = mid+1;
+            }
         }
 
         return -1;
     }
 }
+
+// public class Solution extends GuessGame {
+//     public int guessNumber(int n) {
+//         int low = 0;
+//         int high = n;
+
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+
+//             if (guess(mid) == 0)
+//                 return mid;
+//             if (guess(mid) == -1)
+//                 high = mid - 1;
+//             if (guess(mid) == 1)
+//                 low = mid + 1;
+//         }
+
+//         return -1;
+//     }
+// }
