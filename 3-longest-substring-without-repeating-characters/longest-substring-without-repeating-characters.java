239f8d55b70235df7570edd4daf7aa7a3264ba9f
Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>
2026-02-06 12:18:14 +0530

    Time: 7 ms (36.30%) | Memory: 45.7 MB (71.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,26 +1,35 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        
         int maxLength = 0;
-
-        Map<Character,Integer> map = new HashMap<>();
         int start = 0;
+        Map<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            while(map.get(s.charAt(i)) > 1)
+            char c = s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+            while(map.get(c) > 1)
             {
-                int val = map.get(s.charAt(start));
-                if(val == 1) {
-                    map.remove(s.charAt(start));
-                    start++;
-                }
-                else{
-                    map.put(s.charAt(start),val-1);
-                    start++;
-                }
+                char leftChar = s.charAt(start);
+                int val = map.get(leftChar);
+                if(val == 1) map.remove(leftChar);
+                else map.put(leftChar,val-1);
+                start++;
             }
+            maxLength = Math.max(maxLength,i-start+1);
+            // Set<Character> set = new HashSet<>();
+            // int length = 0;
+            // for(int j=i;j<s.length();j++)
+            // {
+            //     if(set.contains(s.charAt(j)))
+            //     {
+            //         break;
+            //     }
+            //     set.add(s.charAt(j));
+            //     length++;
+            // }
 
-            maxLength = Math.max(i-start+1,maxLength);
+            // maxLength = Math.max(maxLength,length);
         }
 
         return maxLength;
